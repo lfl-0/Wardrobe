4feac8d4c23f71376c108c952fab6ffa8b7df1fd
LiFaliang1996 <274750134@qq.com>
2019-11-18 17:54:42 +0800

    cofig

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-// module.exports = {
-//   devServer: {
-//     host: '192.168.31.69'
-//   }
-// }
-=======
 module.exports = {
   devServer: {
     host: '192.168.31.69'
   }
 }
->>>>>>> f61cea1294fd3339e75e7b757f260378d2c14317
 
